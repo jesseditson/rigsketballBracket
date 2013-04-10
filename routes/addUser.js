c329164e9e0153014dd0f224e4267232ee5820da
Jesse Ditson <jesse.ditson@gmail.com>
2013-04-10 15:17:47 -0700

    move basic auth info to config

--- routes/addUser.js
+++ routes/addUser.js
@@ -1,5 +1,6 @@
 var bracketData = require('../lib/bracketData')
 var express = require('express')
+var config = require('config-heroku')
 
 var renderLogin = function(req,res,next){
   res.render('addUser')
@@ -7,7 +8,7 @@ var renderLogin = function(req,res,next){
 
 module.exports = function(app){
   var auth = express.basicAuth(function(user, pass) {     
-     return (user == "bim" && pass == "thisr1ps");
+     return (user == config.rootuser && pass == config.rootpass);
   },'Log in to add a user')
   app.get('/addUser',auth,renderLogin)
 }
