b6b40dd2fe3d891b8cd266c3463d2e5fd435d4aa
Jesse Ditson <jesse.ditson@gmail.com>
2013-06-01 22:01:26 -0700

    export save to it's own module, save signups to bracket initially selected

--- routes/save.js
+++ routes/save.js
@@ -1,8 +1,8 @@
-var db = require('../lib/mongoWrapper').db.add('bands').add('rounds')
-var async = require('async')
+var save = require('../lib/save')
 var bracketData = require('../lib/bracketData')
 
 var render = function(req,res,err,data){
+  data = data || {}
   bracketData(function(err,info){
     if(err) return res.json({error : err.message})
     res.render('brackets',info,function(err,html){
@@ -13,54 +13,6 @@ var render = function(req,res,err,data){
   })
 }
 
-var save = function(req,res,next){
-  if(req.query.operation == 'saveBand'){
-    db.bands.findOne({position : req.query.position},function(err,pBand){
-      if(err) return render(req,res,err)
-      if(pBand && !req.user) return render(req,res,new Error("You're not allowed to do that."))
-      if(pBand){
-        // editing. only name is editable
-        db.bands.update({position : req.query.position},{$set : {name : req.query.name}},function(err){
-          render(req,res,err,{})
-        })
-      } else {
-        // this is available to anyone.
-        var band = {
-          name : req.query.name,
-          position : req.query.position,
-          created_at : new Date(),
-          scores : {
-            32 : false,
-            16 : false,
-            8 : false,
-            4 : false,
-            2 : false
-          }
-        }
-        db.bands.insert(band,function(err){
-          render(req,res,err,{})
-        })
-      }
-    })
-  } else if(!req.user){
-    // authenticated save commands
-    render(req,res,new Error("You're not allowed to do that."))
-  } else if(req.query.operation == 'editScore') {
-    var up = {}
-    up['scores.' + req.query.round] = req.query.score
-    db.bands.update({position : req.query.position},{$set : up},function(err){
-      render(req,res,err,{})
-    })
-  } else if(req.query.operation == 'setInfo'){
-    var up = {}
-    if(req.query.location) up.location = req.query.location
-    if(req.query.date) up.date = req.query.date
-    db.rounds.update({round : parseInt(req.query.round,10), position : parseInt(req.query.position,10)},{$set : up},function(err,updated){
-      render(req,res,err,{})
-    })
-  }
-}
-
 module.exports = function(app){
-  app.get('/save',save)
+  app.get('/save',save.bind({render : render}))
 }
--- routes/signup.js
+++ routes/signup.js
@@ -1,17 +1,21 @@
 var bracketData = require('../lib/bracketData')
+var save = require('../lib/save')
 
 var render = function(req,res,next){
+  if(!req.query.bandNum) return res.redirect('/')
   req.session.signingup = true
-  res.render('signup')
+  res.render('signup',{bandNum : req.query.bandNum})
 }
 
 var submit = function(req,res,next){
-  if(!req.session.signingup) res.redirect('/signup')
-  delete req.session.signingup
-  bracketData(true,function(err,info){
-    if(err) return res.json({error : err.message})
-    res.render("brackets",info)
-  })
+  //if(!req.session.signingup || !req.query.bandNum) return res.redirect('/')
+  //delete req.session.signingup
+  req.query.operation = 'saveBand'
+  save.call({
+    render : function(req,res,err,data){
+      res.redirect('/')
+    }
+  },req,res,next)
 }
 
 module.exports = function(app){
